ea7b154ac99c50c4d18ee3ef454950d806c82dee
Sean Dever <seand0611@gmail.com>
2020-03-06 23:31:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,13 +13,14 @@ function nowServing(katzDeliLine) {
   'There is nobody waiting to be served!'
 }
 
+
 function currentLine(katzDeliLine) {
   let line = [];
   let count = 0;
   if (katzDeliLine.length > 0) {
     for(let i of katzDeliLine) {
       count++;
-      line.push(count + ". " + i)
+      line.push(`${count}. ${i}`)
     } return `The line is currently: ${line.join(', ')}`;
   } else {
     return "The line is currently empty."
